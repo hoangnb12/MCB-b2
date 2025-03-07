0c14103fb49edbbe7f93a00236a3d05e68cac294
hoangnb12 <kedaumatnb@gmail.com>
2025-03-07 08:28:34 +0700

    first commit

---
+++ Objects/misc.d
@@ -0,0 +1,20 @@
+./objects/misc.o: \
+  G:\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\src\misc.c \
+  G:\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\misc.h \
+  G:\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
+  RTE\_Target_1\RTE_Components.h \
+  G:\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\core_cm3.h \
+  D:\hoc\ tap\file\ ung\ dung\ lam\ bai\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  G:\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_version.h \
+  G:\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_compiler.h \
+  G:\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_armclang.h \
+  D:\hoc\ tap\file\ ung\ dung\ lam\ bai\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  D:\hoc\ tap\file\ ung\ dung\ lam\ bai\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  G:\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h \
+  RTE\Device\STM32F103C8\stm32f10x_conf.h \
+  G:\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h \
+  G:\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_i2c.h \
+  G:\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h \
+  G:\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_spi.h \
+  G:\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_tim.h \
+  G:\ARM\PACK\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_usart.h
